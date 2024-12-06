41814084aa44bb326dde515b0046e5af73f7f578
Ingrid Vianna <ingrid.vianna1@outlook.com>
2024-12-05 21:41:02 -0300

    :pencil: update js

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,6 +1,15 @@
+
+function handleMouseEnter() {
+  this.classList.add('s-card--hovered');
+}
+
 function addEventListinersToCards() {
   const cardElements = document.getElementsByClassName('s-card');
-  console.log(cardElements);
+  
+  for (let i = 0; i < cardElements.length; i++) {
+    const card = cardElements[i];
+    card.addEventListener('mouseenter', handleMouseEnter);
+  }
 }
 
 document.addEventListener("DOMContentLoaded", addEventListinersToCards, false);
